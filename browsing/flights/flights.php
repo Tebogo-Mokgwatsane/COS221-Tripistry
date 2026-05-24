a17ff9b4966862c38975b9349ad5a2edf1608a2e
EmCee-R <mcranks03@gmail.com>
2026-05-24 21:33:45 +0200

    deleted useless flights.html

--- browsing/flights/flights.php
+++ browsing/flights/flights.php
@@ -7,7 +7,27 @@
  header('Content-Type: application/json');
  echo json_encode($flights);
 
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flights</title>
+    <link rel="stylesheet" href="flights.css">
+    <link rel="stylesheet" href="../../css/navbar.css">
+
+</head>
+<body>
+    <?php include ("../../navbar.php"); ?>
+    <div class="flight-grid" id="flights">
+    </div>
+
+</body>
+<script src="flights.js"></script>
+</html>
 
 
 
