7a5285e8df5fd31d094c44d7975bdd9ba7e6f115
EmCee-R <mcranks03@gmail.com>
2026-05-26 05:28:03 +0200

    updated browse

--- browse/attractions/attractions.php
+++ browse/attractions/attractions.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tripistry - Attractions</title>
-    <!--<link rel="stylesheet" href="attractions.css">-->
+    <link rel="stylesheet" href="attractions.css">
     <link rel="stylesheet" href="../../css/navbar.css">
 </head>
 <body>
