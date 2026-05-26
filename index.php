ad19f5d0ec38230d136cfbb090cbaf9fea0e2810
Master P <manqoba.mashego@imaginescholar.org>
2026-05-26 20:57:02 +0200

    signup.js

--- index.php
+++ index.php
@@ -160,24 +160,7 @@
         </div>
     </div>
 
-    <?php
-
-    require_once 'config.php';
-
-$conn = new mysqli(
-    getenv('DB_HOST'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_NAME')
-);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully!";
-
-?>
+   
     <footer>
         &copy; 2026 Tripistry. All rights reserved.
     </footer>
